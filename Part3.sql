607661528f199ca6554a2fad94e0c9fe4b1073e8
Ivan Barros <ivan.dariobc@hotmail.com>
2023-07-28 21:29:33 -0400

    Update Part3.sql

--- Part3.sql
+++ Part3.sql
@@ -1,19 +1,29 @@
+BEGIN TRANSACTION
 UPDATE Caso
-SET Horas_trabajadas = dbo.Calcular(Fecha_inicio,Fecha_fin,Horai_bodega,Horaf_bodega, Horai,Horaf);
-
+SET Horas_trabajadas = dbo.Calcular(Fecha_inicio,Fecha_fin,Horai_bodega,Horaf_bodega, Horai,Horaf)
+WHERE (Fecha_fin != '' or Fecha_fin != '' );
 ------------------------------------------------------------------------------
+UPDATE Caso
+SET Horas = CONVERT(DECIMAL(10,2), dbo.Calcular(Fecha_inicio, Fecha_fin, Horai_bodega, Horaf_bodega, Horai, Horaf) / 3600.0)
+WHERE (Fecha_fin != '' or Fecha_fin != '' );
+
+
 UPDATE caso SET Horas_trabajadas = CONCAT(FORMAT(Horas_trabajadas / 3600, '00'), ':', 
                               RIGHT('0' + FORMAT(Horas_trabajadas % 3600 / 60, '00'), 2), ':',
-                              RIGHT('0' + FORMAT(Horas_trabajadas % 60, '00'), 2));
+                              RIGHT('0' + FORMAT(Horas_trabajadas % 60, '00'), 2))
+							  WHERE (Fecha_fin != '' or Fecha_fin != '' );
 
+COMMIT TRANSACTION
 select * from Caso
 
---DECLARE @fechaInicio DATE = '2023-01-03';
---DECLARE @fechaFin DATE = '2023-01-04';
+
+--DECLARE @fechaInicio DATE = '2023-05-11';
+--DECLARE @fechaFin DATE = '2023-05-19';
 --DECLARE @horaInicio1 TIME = '06:30:00';
 --DECLARE @horaFin1 TIME = '16:30:00';
---DECLARE @horaInicio2 TIME = '16:29:00';
---DECLARE @horaFin2 TIME = '06:31:00';
+--DECLARE @horaInicio2 TIME = '11:11:00';
+--DECLARE @horaFin2 TIME = '08:55:16';
 
---SElect dbo.Calcular(@fechaInicio, @fechaFin, @horaInicio1, @horaFin1, @horaInicio2, @horaFin2) as CantiSegTotal;
+--SElect CONVERT(DECIMAL(10,2),dbo.Calcular(@fechaInicio, @fechaFin, @horaInicio1, @horaFin1, @horaInicio2, @horaFin2)/ 3600.0) as Result;
+
 
